7419e8f8a40affba72e9b407b7e784381f35ef70
cnsheafe <cnsheafe@gmail.com>
2017-07-17 09:10:55 -0500

    Added mock-api calls, more features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service'
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component'
+import { DashboardComponent } from './dashboard.component'
+import { HeroSearchComponent } from './hero-search.component'
+
+import HeroService from './hero.service';
+import AppRoutingModule from './app-routing.module'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent,
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
